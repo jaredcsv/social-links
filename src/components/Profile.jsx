9d789398cc6a09f3ac5f73bb8af2b268bf23f4bc
JaredMZ <jaredcv04@gmail.com>
2024-01-18 20:51:16 -0600

    pfp update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -33,7 +33,7 @@ function Profile() {
             <div className="profile-pic-container">
               <img
                 className="profile-pic"
-                src="/assets/img/pfp-jared.jpeg"
+                src="/src/assets/img/pfp-jared.jpeg"
                 alt="profile pic"
               />
             </div>
